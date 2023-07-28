e766cbaaeb469158844c0117e3b4e643532d575a
Miura Kosuke <being.paperlefthand@gmail.com>
2023-07-28 22:36:08 +0900

    Update __init__.py

--- pygourmet/__init__.py
+++ pygourmet/__init__.py
@@ -3,4 +3,3 @@ __version__ = "0.1.0"
 from .api import Api  # noqa: F401
 from .coordinates import Coordinates  # noqa: F401
 from .exceptions import PyGourmetError  # noqa: F401
-from .options import Options  # noqa: F401
